321c71c871c222d551d30426b0b60d1fac08ba55
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-21 23:45:42 +0530

    Final Commit

--- controllers/instaController.js
+++ controllers/instaController.js
@@ -11,24 +11,21 @@ exports.download = async function (req, res, next) {
         })
         const $ = cheerio.load(html.data)
 
-        // const videoLink = $("meta[property='og:video']").attr("content")
-        // const poster = $("meta[property='og:image']").attr("content")
-        // const type = $("meta[property='og:type']").attr("content")
-        // const site = $("meta[property='og:site_name']").attr("content")
-        // const description = $("meta[property='og:description']").attr("content")
+        const videoLink = $("meta[property='og:video']").attr("content")
+        const poster = $("meta[property='og:image']").attr("content")
+        const type = $("meta[property='og:type']").attr("content")
+        const site = $("meta[property='og:site_name']").attr("content")
+        const description = $("meta[property='og:description']").attr("content")
 
-        const description = $("meta[name='robots']").attr("content")
-        // res.locals.data = {
-        //     videoLink,
-        //     type,
-        //     site,
-        //     poster,
-        //     description
-        // }
-        res.status(200).json({
+
+        res.locals.data = {
+            videoLink,
+            type,
+            site,
+            poster,
             description
-        })
-        //next()
+        }
+        next()
     } catch (e) {
         console.log('axiosError:', e.message)
         res.status(200).json({
