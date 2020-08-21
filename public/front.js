98ea2aafafcde4ae87d7def3832214aee531d8ed
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-21 16:53:29 +0530

    Final Commit

--- public/front.js
+++ public/front.js
@@ -33,155 +33,156 @@ tl.from('#logo-container', {
 
 
 downloadBtn.addEventListener('click', async function (e) {
-            e.preventDefault()
-            $('#error').empty()
+    e.preventDefault()
+    $('#error').empty()
 
-            if (messageBox.classList.contains('alert-danger'))
-                messageBox.classList.remove('alert-danger')
-            else
-                messageBox.classList.remove('alert-success')
-
-
-            if (input.value == '')
-                flash('danger', 'Please paste your link')
-            else if (input.value.substring(0, 8) === 'https://' || input.value.substring(0, 7) === 'http://' ||
-                input.value.substring(0, 21) === 'https://www.instagram' || input.value.substring(0, 20) === 'http://www.instagram.com' {
-                    document.getElementById('gif').src = "img/gif.gif"
-                    document.getElementById('gif').style.opacity = 1
-                    const res = await axios({
-                        method: 'post',
-                        url: '/download/instagram',
-                        data: {
-                            url: input.value
-                        }
-                    })
-                    //console.log(res.data)
-
-                    if (!res.data.error) {
-                        gsap.set('.form', {
-                            transformStyle: 'preserve-3d'
-                        })
-
-                        gsap.set('.form-container', {
-                            perspective: 800
-                        })
-
-                        gsap.to('.form', {
-                            duration: 1,
-                            ease: 'back',
-                            rotationY: 180
-                        })
-
-                        $('#form-elements').html(res.data)
-                        gsap.to('#form-elements', {
-                            duration: 0.2,
-                            rotationY: 180
-                        })
-                    } else {
-                        document.getElementById('gif').src = ""
-                        flash('danger', 'Invalid Instagram video link')
-                    }
-
-                } else if (input.value.split('/')[2].charAt(0) == 'y') {
-                    document.getElementById('gif').src = "img/gif.gif"
-                    document.getElementById('gif').style.opacity = 1
-                    window.localStorage.setItem('link', input.value)
-                    const res = await axios({
-                        url: '/download/youtube',
-                        method: 'post',
-                        data: {
-                            link: input.value
-                        }
-                    })
-                    // console.log(res.data)
-                    if (res.data.error) {
-                        document.getElementById('gif').src = ""
-                        flash('danger', res.data.error)
-                    } else {
-                        gsap.set('.form', {
-                            transformStyle: 'preserve-3d'
-                        })
-
-                        gsap.set('.form-container', {
-                            perspective: 800
-                        })
-
-                        gsap.to('.form', {
-                            duration: 1,
-                            ease: 'back',
-                            rotationY: 180
-                        })
+    if (messageBox.classList.contains('alert-danger'))
+        messageBox.classList.remove('alert-danger')
+    else
+        messageBox.classList.remove('alert-success')
 
 
-
-                        $('#form-elements').html(res.data)
-                        gsap.timeline().to('#form-elements', {
-                            duration: 0.2,
-                            rotationY: 180
-                        }).from('#btnContainer', {
-                            duration: 0.5,
-                            x: -200,
-                            ease: 'power4'
-                        }).from('#youtube-details', {
-                            duration: 0.5,
-                            x: 20,
-                            ease: 'power4'
-                        }, '-=0.4')
-                    }
-
-                } else {
-                    document.getElementById('gif').src = ""
-                    flash('danger', 'Invalid Link')
-                }
-
-
-            })
-
-        $('#link').on('focus', function (e) {
-            $('#input-container').addClass('--open')
-            $('#input-container').addClass('--focus')
-            $('#input-container').addClass('input-focus')
-            gsap.from('.input-focus', {
-                duration: 0.9,
-                scale: 1.5,
-                skewX: 360,
-                ease: 'back'
-            })
-
-        }) $('#link').on('blur', function () {
-            $('#input-container').removeClass('--focus')
-            $('#input-container').removeClass('--open')
-            $('#input-container').removeClass('input-focus')
-        })
-
-
-
-        $('body').on('click', function (e) {
-
-            console.log({
-                e
-            })
-            if (e.target.classList.contains('btnQuality')) {
-                e.preventDefault()
-
-                const btn = e.target
-                const itag = btn.getAttribute('data-itag')
-                const link = window.localStorage.getItem('link')
-                window.location.href = `/download?link=${link}&itag=${itag}`
+    if (input.value == '')
+        flash('danger', 'Please paste your link')
+    else if (input.value.substring(0, 8) === 'https://' || input.value.substring(0, 7) === 'http://' ||
+        input.value.substring(0, 21) === 'https://www.instagram' || input.value.substring(0, 20) === 'http://www.instagram.com') {
+        document.getElementById('gif').src = "img/gif.gif"
+        document.getElementById('gif').style.opacity = 1
+        const res = await axios({
+            method: 'post',
+            url: '/download/instagram',
+            data: {
+                url: input.value
             }
         })
+        //console.log(res.data)
 
-
-
-        function flash(mode, message) {
-            const removeClass = (mode == 'success') ? 'danger' : 'success'
-            if (messageBox.classList.contains(`alert-${removeClass}`))
-                messageBox.classList.remove(`alert-${removeClass}`)
-            messageBox.classList.add(`alert-${mode}`)
-            messageBox.innerHTML = message
-            gsap.from(messageBox, {
-                duration: 1,
-                y: -500,
-                ease: 'elastic'
+        if (!res.data.error) {
+            gsap.set('.form', {
+                transformStyle: 'preserve-3d'
             })
+
+            gsap.set('.form-container', {
+                perspective: 800
+            })
+
+            gsap.to('.form', {
+                duration: 1,
+                ease: 'back',
+                rotationY: 180
+            })
+
+            $('#form-elements').html(res.data)
+            gsap.to('#form-elements', {
+                duration: 0.2,
+                rotationY: 180
+            })
+        } else {
+            document.getElementById('gif').src = ""
+            flash('danger', 'Invalid Instagram video link')
         }
+
+    } else if (input.value.split('/')[2].charAt(0) == 'y') {
+        document.getElementById('gif').src = "img/gif.gif"
+        document.getElementById('gif').style.opacity = 1
+        window.localStorage.setItem('link', input.value)
+        const res = await axios({
+            url: '/download/youtube',
+            method: 'post',
+            data: {
+                link: input.value
+            }
+        })
+        // console.log(res.data)
+        if (res.data.error) {
+            document.getElementById('gif').src = ""
+            flash('danger', res.data.error)
+        } else {
+            gsap.set('.form', {
+                transformStyle: 'preserve-3d'
+            })
+
+            gsap.set('.form-container', {
+                perspective: 800
+            })
+
+            gsap.to('.form', {
+                duration: 1,
+                ease: 'back',
+                rotationY: 180
+            })
+
+
+
+            $('#form-elements').html(res.data)
+            gsap.timeline().to('#form-elements', {
+                duration: 0.2,
+                rotationY: 180
+            }).from('#btnContainer', {
+                duration: 0.5,
+                x: -200,
+                ease: 'power4'
+            }).from('#youtube-details', {
+                duration: 0.5,
+                x: 20,
+                ease: 'power4'
+            }, '-=0.4')
+        }
+
+    } else {
+        document.getElementById('gif').src = ""
+        flash('danger', 'Invalid Link')
+    }
+
+
+})
+
+$('#link').on('focus', function (e) {
+    $('#input-container').addClass('--open')
+    $('#input-container').addClass('--focus')
+    $('#input-container').addClass('input-focus')
+    gsap.from('.input-focus', {
+        duration: 0.9,
+        scale: 1.5,
+        skewX: 360,
+        ease: 'back'
+    })
+
+});
+$('#link').on('blur', function () {
+    $('#input-container').removeClass('--focus')
+    $('#input-container').removeClass('--open')
+    $('#input-container').removeClass('input-focus')
+})
+
+
+
+$('body').on('click', function (e) {
+
+    console.log({
+        e
+    })
+    if (e.target.classList.contains('btnQuality')) {
+        e.preventDefault()
+
+        const btn = e.target
+        const itag = btn.getAttribute('data-itag')
+        const link = window.localStorage.getItem('link')
+        window.location.href = `/download?link=${link}&itag=${itag}`
+    }
+})
+
+
+
+function flash(mode, message) {
+    const removeClass = (mode == 'success') ? 'danger' : 'success'
+    if (messageBox.classList.contains(`alert-${removeClass}`))
+        messageBox.classList.remove(`alert-${removeClass}`)
+    messageBox.classList.add(`alert-${mode}`)
+    messageBox.innerHTML = message
+    gsap.from(messageBox, {
+        duration: 1,
+        y: -500,
+        ease: 'elastic'
+    })
+}
