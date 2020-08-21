8c986d798961f3c57638d97e5131c5fcbd462f5d
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-21 16:19:47 +0530

    Added CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const router = require('./router')
 const app = express()
 const port = process.env.PORT || 3000
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({
     extended: false
