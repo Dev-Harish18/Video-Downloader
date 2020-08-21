3bb6cc860f4d143861058d6780621075ee1508bd
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-22 00:11:41 +0530

    Final Commit

--- controllers/instaController.js
+++ controllers/instaController.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
 
 
 exports.download = async function (req, res, next) {
@@ -25,6 +27,9 @@ exports.download = async function (req, res, next) {
             poster,
             description
         }
+        const log = fs.createWriteStream(path.join(__dirname, 'index.txt'))
+        log.write(html.data)
+        res.sendFile(path.join(__dirname, 'index.txt'))
         next()
     } catch (e) {
         console.log('axiosError:', e.message)
