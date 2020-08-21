04880d5b0168d0dc44266ee2f354ff339506572c
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-21 22:11:00 +0530

    Final Commit

--- controllers/instaController.js
+++ controllers/instaController.js
@@ -7,10 +7,7 @@ exports.download = async function (req, res, next) {
     try {
         const html = await axios({
             method: 'get',
-            url: req.body.url,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'
-            }
+            url: req.body.url
         })
         const $ = cheerio.load(html.data)
 
@@ -19,13 +16,20 @@ exports.download = async function (req, res, next) {
         const type = $("meta[property='og:type']").attr("content")
         const site = $("meta[property='og:site_name']").attr("content")
         const description = $("meta[property='og:description']").attr("content")
-        res.locals.data = {
+        // res.locals.data = {
+        //     videoLink,
+        //     type,
+        //     site,
+        //     poster,
+        //     description
+        // }
+        res.status(200).json({
             videoLink,
             type,
             site,
             poster,
             description
-        }
+        })
         next()
     } catch (e) {
         console.log('axiosError:', e.message)
