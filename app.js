1debbfa3927dc0c33590ccaf35c83cd51a14d31b
Hacker-Harish-483 <harishvilar91@gmail.com>
2020-08-23 11:53:45 +0530

    Final Commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const router = require('./router')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({
